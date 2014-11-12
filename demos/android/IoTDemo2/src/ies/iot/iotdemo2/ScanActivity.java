9cd1668afa42297bbac79aa947cb92acdafcd868
Lu Ken <bluewish.ken.lu@gmail.com>
2014-11-12 23:34:18 +0800

    finish demo2 scan activity

--- demos/android/IoTDemo2/src/ies/iot/iotdemo2/ScanActivity.java
+++ demos/android/IoTDemo2/src/ies/iot/iotdemo2/ScanActivity.java
@@ -1,13 +1,97 @@
 package ies.iot.iotdemo2;
 
+import ies.iot.demolib.ble.BeaconScanInfo;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.bluetooth.BluetoothDevice;
 
 public class ScanActivity extends Activity {
+    private final String TAG = getClass().getSimpleName();
+    private Context mContext;
+    private SensorTagService mService;
+    private ListView mListScan;
+    private ScanListAdapter mListScanAdapter;
+    private Handler mDisappearCheckHandler;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+        mContext = this;
+        mListScan = (ListView)findViewById(R.id.lv_scan);
+        mListScanAdapter = new ScanListAdapter(this);
+        mListScan.setAdapter(mListScanAdapter);
+        connectService();
+        
+        mDisappearCheckHandler = new Handler();
+        mDisappearCheckHandler.postDelayed(mDisappearCheckRunner, 1000);
     }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDisappearCheckHandler.removeCallbacks(mDisappearCheckRunner);
+        disconnectService();
+    }
+    
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            Log.v(TAG, "onServiceConnected.");
+            Toast.makeText(mContext, "Service connected", 
+                    Toast.LENGTH_SHORT).show();
+            SensorTagService.SensorTagServiceBinder b = 
+                    (SensorTagService.SensorTagServiceBinder)binder;
+            mService = b.getService();
+            mService.getBleManager().startScan(mScanCallbackHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.v(TAG, "onServiceDisconnected."); 
+            Toast.makeText(mContext, "Service disconnected", 
+                    Toast.LENGTH_SHORT).show();            
+        }
+        
+    };
+    
+    public void connectService() {
+        Intent intent = new Intent(this, SensorTagService.class);
+        startService(intent);
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
+    }
+    
+    public void disconnectService() {
+        unbindService(mConnection);
+    }
+    
+    private Handler mScanCallbackHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mListScanAdapter.update_device((BluetoothDevice)msg.obj, msg.arg1);
+        }
+    };
+    
+    private Runnable mDisappearCheckRunner = new Runnable() {
+        @Override
+        public void run() {
+            mListScanAdapter.refresh_disappeared_device();
+            mDisappearCheckHandler.postDelayed(mDisappearCheckRunner, 1000);
+        }
+        
+    };
 }
--- demos/android/IoTDemo2/src/ies/iot/iotdemo2/SensorTagService.java
+++ demos/android/IoTDemo2/src/ies/iot/iotdemo2/SensorTagService.java
@@ -1,15 +1,35 @@
 package ies.iot.iotdemo2;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import ies.iot.demolib.ble.*;
 
 public class SensorTagService extends Service {
     private final String TAG = getClass().getSimpleName();
-    
+    private BleManager mBleManager;
     private final IBinder mServiceBinder = new  SensorTagServiceBinder();
+    private NotificationCompat.Builder mNotificationBuilder;
+   
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "onCreate");
+        
+        mBleManager = new BleManager(this, mStateHandler);
+        mNotificationBuilder = new NotificationCompat.Builder(this)
+            .setContentTitle("IoTDemo2")
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setOngoing(true);
+        
+        setNotification("Service is starting");
+    }
     
     @Override
     public IBinder onBind(Intent intent) {
@@ -19,6 +39,10 @@ public class SensorTagService extends Service {
     @Override
     public boolean onUnbind(Intent intent) {
         Log.v(TAG, "onUnbind");
+        if (mBleManager.getState() == BleManager.STATE_SCANNING) {
+            mBleManager.stopScan();
+        }
+        
         return true;
     }
     
@@ -34,5 +58,32 @@ public class SensorTagService extends Service {
        Log.v(TAG, "onStartCommand");
        return START_STICKY;
     }
+    
+    public BleManager getBleManager() {
+        return mBleManager;
+    }
+    
+    public void setNotification(String message) {
+        mNotificationBuilder.setContentText(message);
+        NotificationManager notificationManager = 
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mNotificationBuilder.build());        
+    }
 
+    private Handler mStateHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int state = msg.arg1;
+            if (state == BleManager.STATE_SCANNING) {
+                setNotification("BLE scanning");
+            } else if (state == BleManager.STATE_DISCONNECTED) {
+                setNotification("BLE disconnected");
+            } else if (state == BleManager.STATE_CONNECTING) {
+                setNotification("BLE connecting");
+            } else if (state == BleManager.STATE_CONNECTED) {
+                setNotification("BLE connected");
+            }
+        }
+    };
+    
 }
